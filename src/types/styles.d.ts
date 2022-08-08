bfb558afb28167b2ca80d3c65bd1e476f53302e4
Acasune <tttt4aaaa@gmail.com>
2022-08-08 16:01:31 +0900

    list6.6-7 adding toPropValues

---
+++ src/types/styles.d.ts
@@ -0,0 +1,125 @@
+﻿/**
+ * Responsiveプロパティ
+ * CSSプロパティの値をブレークポイントごとに設定できる
+ * TはCSSプロパティの値の型
+ */
+export type ResponsiveProp<T> = {
+  base: T // デフォルト
+  sm?: T // 640px以上
+  md?: T // 768px以上
+  lg?: T // 1024px以上
+  xl?: T // 2048px以上
+}
+export type Responsive<T> = T | ResponsiveProp<T>
+
+/**
+ * Flex
+ */
+type SelfPosition =
+  | 'center'
+  | 'end'
+  | 'flex-end'
+  | 'flex-start'
+  | 'self-end'
+  | 'self-start'
+  | 'start'
+
+type ContentPosition = 'center' | 'end' | 'flex-end' | 'flex-start' | 'start'
+
+type ContentDistribution =
+  | 'space-around'
+  | 'space-between'
+  | 'space-evenly'
+  | 'stretch'
+
+type CSSPropertyGlobals =
+  | '-moz-initial'
+  | 'inherit'
+  | 'initial'
+  | 'revert'
+  | 'unset'
+
+export type CSSPropertyAlignItems =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'baseline'
+  | 'normal'
+  | 'stretch'
+
+export type CSSPropertyAlignContent =
+  | CSSPropertyGlobals
+  | ContentDistribution
+  | 'center'
+  | 'end'
+  | 'flex-end'
+  | 'flex-start'
+  | 'start'
+  | 'baseline'
+  | 'normal'
+
+export type PropertyJustifyItems =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'baseline'
+  | 'left'
+  | 'legacy'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+
+export type CSSPropertyJustifyContent =
+  | CSSPropertyGlobals
+  | ContentDistribution
+  | ContentPosition
+  | 'left'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+
+export type CSSPropertyFlexWrap =
+  | CSSPropertyGlobals
+  | 'nowrap'
+  | 'wrap'
+  | 'wrap-reverse'
+
+export type CSSSPropertyFlexDirection =
+  | CSSPropertyGlobals
+  | 'column'
+  | 'column-reverse'
+  | 'row'
+  | 'row-reverse'
+
+export type CSSPropertyJustifySelf =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'auto'
+  | 'baseline'
+  | 'left'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+
+export type CSSPropertyAlignSelf =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'auto'
+  | 'baseline'
+  | 'normal'
+  | 'stretch'
+
+/**
+ * Grid
+ */
+type GridLine = 'auto'
+
+export type CSSPropertyGridColumn = CSSPropertyGlobals | GridLine
+
+export type CSSPropertyGridRow = CSSPropertyGlobals | GridLine
+
+export type CSSPropertyGridAutoFlow =
+  | CSSPropertyGlobals
+  | 'column'
+  | 'dense'
+  | 'row'
+
+export type CSSPropertyGridArea = CSSPropertyGlobals | GridLine
