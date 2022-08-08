b0c4aa4a2fa3747aec28f5edcb6655cb424957d4
Acasune <tttt4aaaa@gmail.com>
2022-08-08 13:46:36 +0900

    list6.1-4 introducing api client

---
+++ src/types/data.d.ts
@@ -0,0 +1,31 @@
+﻿// 商品カテゴリ
+export type Categori = 'shoes' | 'clothes' | 'book'
+// 商品の状態
+export type Condition = 'new' | 'used'
+
+// ユーザー
+export type User = {
+  id: number
+  username: stirng
+  displayName: string
+  email: string
+  prifileImageUrl: string
+  description: string
+}
+
+export type Product = {
+  id: number
+  categori: Category
+  title: string
+  description: string
+  imageUrl: string
+  blurDataUrl: string
+  price: number
+  condition: Condition
+  owner: User
+}
+
+// API Context
+export type ApiContext = {
+  apiRootUrl: string
+}
