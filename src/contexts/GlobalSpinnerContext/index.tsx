7f8727095e20ad4528f9a8363c5d2a2aaafa35b7
Acasune <tttt4aaaa@gmail.com>
2022-08-09 23:52:55 +0900

    list6.33-35 adding organisms/globalspinner

---
+++ src/contexts/GlobalSpinnerContext/index.tsx
@@ -0,0 +1,42 @@
+﻿import React, { useContext, useState, createContext } from 'react'
+
+const GlobalSpinnerContext = createContext<boolean>(false)
+const GlobalSpinnerActionsContext = createContext<
+  React.Dispatch<React.SetStateAction<boolean>>
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+>(() => {})
+
+// On/Off of showing global spinner
+export const useGlobalSpinnerContext = (): boolean =>
+  useContext<boolean>(GlobalSpinnerContext)
+
+// Action of On/Off of showing global spinner
+export const useGlobalSpinnerActionsContext = (): React.Dispatch<
+  React.SetStateAction<boolean>
+> =>
+  useContext<React.Dispatch<React.SetStateAction<boolean>>>(
+    GlobalSpinnerActionsContext,
+  )
+
+interface GlobalSpinnerContextProviderProps {
+  children?: React.ReactNode
+}
+
+/**
+ * Global spinner context provider
+ */
+const GlobalSpinnerContextProvider = ({
+  children,
+}: GlobalSpinnerContextProviderProps) => {
+  const [isGlobalSpinnerOn, setGlobalSpinner] = useState(false)
+
+  return (
+    <GlobalSpinnerContext.Provider value={isGlobalSpinnerOn}>
+      <GlobalSpinnerActionsContext.Provider value={setGlobalSpinner}>
+        {children}
+      </GlobalSpinnerActionsContext.Provider>
+    </GlobalSpinnerContext.Provider>
+  )
+}
+
+export default GlobalSpinnerContextProvider
