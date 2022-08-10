6d96e786c935d32f61051d1c2435d7b5acc2605a
Acasune <tttt4aaaa@gmail.com>
2022-08-10 20:31:45 +0900

    list6.50-51 adding signin page

---
+++ src/containers/SigninFormContainer.tsx
@@ -0,0 +1,36 @@
+﻿import SigninForm from 'components/organisms/SigninForm'
+import { useAuthContext } from 'contexts/AuthContext'
+import { useGlobalSpinnerActionsContext } from 'contexts/GlobalSpinnerContext'
+
+interface SigninFormContainerProps {
+  /**
+   * Event handler that called in sign in
+   */
+  onSignin: (error?: Error) => void
+}
+
+/**
+ * Sign-in form container
+ */
+const SigninFormContainer = ({ onSignin }: SigninFormContainerProps) => {
+  const { signin } = useAuthContext()
+  const setGlobalSpinner = useGlobalSpinnerActionsContext()
+  // Event handler when sign-in button pushed
+  const handleSignin = async (username: string, password: string) => {
+    try {
+      setGlobalSpinner(true)
+      await signin(username, password)
+      onSignin && onSignin()
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        window.alert(err.message)
+        onSignin && onSignin(err)
+      }
+    } finally {
+      setGlobalSpinner(false)
+    }
+  }
+
+  return <SigninForm onSignin={handleSignin} />
+}
+export default SigninFormContainer
---
+++ src/pages/signin.tsx
@@ -0,0 +1,45 @@
+﻿import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import AppLogo from 'components/atoms/AppLogo'
+import Box from 'components/layout/Box'
+import Flex from 'components/layout/Flex'
+import Layout from 'components/templates/Layout'
+import SigninFormContainer from 'containers/SigninFormContainer'
+
+const SigninPage: NextPage = () => {
+  const router = useRouter()
+  const handleSignin = async (err?: Error) => {
+    if (!err) {
+      const redirectTo = (router.query['redirect_to'] as string) ?? '/'
+      console.log('Redirecting', redirectTo)
+      await router.push(redirectTo)
+    }
+  }
+
+  return (
+    <Layout>
+      <Flex
+        paddingTop={2}
+        paddingBottom={2}
+        paddingLeft={{ base: 2, md: 0 }}
+        paddingRight={{ base: 2, md: 0 }}
+      >
+        <Flex width="400px" flexDirection="column" justifyContent="center">
+          <Box marginBottom={2}>
+            <AppLogo />
+          </Box>
+          <Box width="100%">
+            {/*
+                Sign-in form container
+                Call auth api by username and password in form
+                and return onSignin's call back
+            */}
+            <SigninFormContainer onSignin={handleSignin} />
+          </Box>
+        </Flex>
+      </Flex>
+    </Layout>
+  )
+}
+
+export default SigninPage
