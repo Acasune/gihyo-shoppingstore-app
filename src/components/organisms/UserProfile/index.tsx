9c5e1ad4238108e15860bc698b47571fa58c02bc
Acasune <tttt4aaaa@gmail.com>
2022-08-10 16:46:52 +0900

    list6.47-48 adding organisms/userprofile

---
+++ src/components/organisms/UserProfile/index.tsx
@@ -0,0 +1,88 @@
+﻿import ShapeImage from 'components/atoms/ShapeImage'
+import Text from 'components/atoms/Text'
+import Box from 'components/layout/Box'
+import Flex from 'components/layout/Flex'
+
+interface UserProfileProps {
+  /**
+   * variant (display style)
+   */
+  variant?: 'normal' | 'small'
+  /**
+   * Username
+   */
+  username: string
+  /**
+   * user image url
+   */
+  profileImageUrl: string
+  /**
+   * the number of products user owns
+   */
+  numberOfProducts: number
+  /**
+   * user description
+   */
+  description?: string
+}
+
+/**
+ * User profile
+ */
+const UserProfile = ({
+  variant = 'normal',
+  username,
+  profileImageUrl,
+  numberOfProducts,
+  description,
+}: UserProfileProps) => {
+  const profileImageSize = variant === 'small' ? '100px' : '120px'
+
+  return (
+    <Flex>
+      <Box minWidth={profileImageSize}>
+        {/* User Image */}
+        <ShapeImage
+          shape="circle"
+          quality="85"
+          src={profileImageUrl}
+          alt={username}
+          height={profileImageSize}
+          width={profileImageSize}
+        />
+      </Box>
+      <Box padding={1}>
+        <Flex
+          height="100%"
+          flexDirection="column"
+          justifyContent="space-between"
+        >
+          <Box>
+            {/* Username */}
+            <Text
+              as="p"
+              fontWeight="bold"
+              variant="mediumLarge"
+              marginTop={0}
+              marginBottom={1}
+            >
+              {username}
+            </Text>
+            {/* n of product being sold */}
+            <Text marginBottom={1} marginTop={0} as="p">
+              {numberOfProducts}点出品済
+            </Text>
+            {/* User description */}
+            {variant === 'normal' && (
+              <Text margin={0} as="p">
+                {description}
+              </Text>
+            )}
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
+  )
+}
+
+export default UserProfile
