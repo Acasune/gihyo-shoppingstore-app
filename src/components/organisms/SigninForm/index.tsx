01f31a0658b71e66d7e659e3e1558a792463f9a0
Acasune <tttt4aaaa@gmail.com>
2022-08-10 16:11:47 +0900

    list6.43-44 adding organisms/signinform

---
+++ src/components/organisms/SigninForm/index.tsx
@@ -0,0 +1,71 @@
+﻿import { useForm } from 'react-hook-form'
+import Button from 'components/atoms/Button'
+import Input from 'components/atoms/Input'
+import Text from 'components/atoms/Text'
+import Box from 'components/layout/Box'
+
+export type SigninFormData = {
+  username: string
+  password: string
+}
+
+interface SigninFormProps {
+  /**
+   * event handler when sign in button pushed
+   */
+  onSignin?: (username: string, password: string) => void
+}
+
+/**
+ * Sign in form
+ */
+const SigninForm = ({ onSignin }: SigninFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SigninFormData>()
+  const onSubmit = (data: SigninFormData) => {
+    const { username, password } = data
+    onSignin && onSignin(username, password)
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box marginBottom={1}>
+        {/* Sign in user's username*/}
+        <Input
+          {...register('username', { required: true })}
+          name="username"
+          type="text"
+          placeholder="ユーザー名"
+          hasError={!!errors.username}
+        />
+        {errors.username && (
+          <Text color="danger" variant="small" paddingLeft={1}>
+            ユーザー名は必須です
+          </Text>
+        )}
+      </Box>
+      <Box marginBottom={2}>
+        {/* Sign in user's Password  */}
+        <Input
+          {...register('password', { required: true })}
+          name="password"
+          type="password"
+          placeholder="パスワード"
+          hasError={!!errors.password}
+        />
+        {errors.password && (
+          <Text color="danger" variant="small" paddingLeft={1}>
+            パスワードは必須です
+          </Text>
+        )}
+      </Box>
+      <Button width="100%" type="submit">
+        サインイン
+      </Button>
+    </form>
+  )
+}
+
+export default SigninForm
